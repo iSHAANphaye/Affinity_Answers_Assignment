45b187d0ba834269ff70895f48cfea7f3e33d9dc
Ishaan Phaye <75660041+iSHAANphaye@users.noreply.github.com>
2023-04-24 23:28:07 +0530

    Add files via upload

---
+++ 4/command.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/bash
+
+url="https://www.amfiindia.com/spages/NAVAll.txt"
+
+temp_file="NAVAll.txt"
+output_file="output.csv"
+temp_file=$(mktemp)
+
+curl -s "$url" > "$temp_file"
+awk -F ';' 'NR > 3 {print $4 "," $5}' "$temp_file" | sed '1s/^/Scheme name,Net asset value\n/' > "$output_file"
+rm "$temp_file"
